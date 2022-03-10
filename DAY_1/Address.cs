6c4145544cc70c9301744a3643accd0145c50eb8
EgorMorozovvv <101330601+EgorMorozovvv@users.noreply.github.com>
2022-03-10 16:27:24 +0300

    Day1

---
+++ DAY_1/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace DAY_1
+{
+    class Address
+    {
+        public string City;
+        public string Street;
+        public uint Number;
+        public Address() { }
+        public Address(string City, string Street, uint Number)
+        {
+            this.City = City;
+            this.Street = Street;
+            this.Number = Number;
+        }
+    }
+}
---
+++ DAY_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DAY_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address addr = new Address();
+            addr.City = "Минск";
+            addr.Street = "Немига";
+            addr.Number = 55;
+
+            Address addr2 = new Address("Полоцк", "Октябрьская", 57);
+
+            Console.WriteLine($"Адрес без конструктора:\nГород: {addr.City}, Улица: {addr.Street}, Номер: {addr.Number}.\n");
+            Console.WriteLine($"Адрес с конструктором:\nГород: {addr2.City}, Улица: {addr2.Street}, Номер: {addr2.Number}.");
+
+            Console.ReadKey();
+        }
+    }
+}
